24837f55e192662de7301da767be80f3be8b8c48
simima228 <150355836+simima228@users.noreply.github.com>
2025-09-17 14:52:22 +0300

    Create hamming.py

---
+++ info/lab/lab2/hamming.py
@@ -0,0 +1,32 @@
+import itertools
+from math import log
+
+
+def decode(bits):
+    first = bits[:]
+    for i in range(int(log(len(bits) + 1, 2))):
+        first[2 ** i - 1] = '0'
+    for i in range(int(log(len(bits) + 1, 2))):
+        n = 0
+        for j in range(0, len(bits), 2 ** (i + 1)):
+            n += sum(map(int, first[2 ** i - 1:][j:j + 2 ** i]))
+        first[2 ** i - 1] = str(n % 2)
+    return first
+
+
+def main():  # работает для любого полного кода хэмминга, примеры: (7,4): 0110011, (15, 11): 011001110010110
+    code = list(input('Введите сообщение: '))
+    dec = decode(code)
+    if dec != code:
+        n = 0
+        for i in range(len(dec)):
+            if dec[i] != code[i]:
+                n += i + 1
+        code[n - 1] = str((int(code[n - 1]) + 1) % 2)
+        print(f'В сообщении была ошибка на {n}-ом бите, правильный вариант: {"".join(code)}')
+    else:
+        print('Ошибок нет')
+
+
+if __name__ == '__main__':
+    main()
