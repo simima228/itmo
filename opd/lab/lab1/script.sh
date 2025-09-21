58e19f750c6aeb6a83d4309c63ecb28797c46355
simima228 <150355836+simima228@users.noreply.github.com>
2025-09-21 15:00:57 +0300

    Create script.sh

---
+++ opd/lab/lab1/script.sh
@@ -0,0 +1,43 @@
+mkdir lab0
+cd lab0
+touch darumaka0
+touch growlithe8
+touch krabby6
+mkdir -p cherubi5/roselia
+mkdir cherubi5/sealeo
+mkdir cherubi5/armaldo
+touch cherubi5/finneon
+touch cherubi5/blitzle
+mkdir -p gabite1/archeops
+mkdir gabite1/piloswine
+touch gabite1/umbreon
+mkdir -p totodile5/lickilicky
+mkdir totodile5/axew
+touch totodile5/omastar
+touch totodile5/zigzagoon
+echo -e 'Живет  Ocean' > cherubi5/finneon
+echo -e 'Развитые способности  Sap\nSipper' > cherubi5/blitzle
+echo -e 'Живет  Cave Desert Mountain' > darumaka0
+echo -e 'Способности\nDark Art Synchronize Trace' > gabite1/umbreon
+echo -e 'Развитые способности\nJustified' > growlithe
+echo -e 'Способности  Torrent Hyper Cutter Shell\nArmor' > krabby6
+echo -e 'Ходы  Ancientpower Bind Body Slam Dive Double-Edge\nEarth Power Icy Wind Iron Defense Knock Off Mud-Slap Rollout Seismic\nToss Sleep Talk Snore Stealth Rock Water Pulse' > totodile5/omastar
+echo -e 'Способности\nGrowl Tackle Tail Whip Headbutt Sand-Attack Odor Sleuth Mud Sport Pin\nMissile Covet Bestow Flail Rest Belly Drum Fling' > totodile5/zigzagoon
+chmod 321 cherubi5
+chmod 752 cherubi5/roselia
+chmod 537 cherubi5/sealeo
+chmod 624 cherubi5/finneon
+chmod 737 cherubi5/armaldo
+chmod 400 cherubi5/blitzle
+chmod 600 darumaka0
+chmod 736 gabite1
+chmod 440 gabite1/umbreon
+chmod 333 gabite1/archeops
+chmod 755 gabite1/piloswine
+chmod 004 growlithe8
+chmod 640 krabby6
+chmod 576 totodile5
+chmod 640 totodile5/omastar
+chmod 751 totodile5/lickilicky
+chmod 044 totodile5/zigzagoon
+chmod 736 totodile5/axew
