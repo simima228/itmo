fec9da7bcd3c551a1ad6a77ff4f8e0e1f9356543
simima228 <150355836+simima228@users.noreply.github.com>
2025-09-29 00:37:20 +0300

    Update script.sh

--- opd/lab/lab1/script.sh
+++ opd/lab/lab1/script.sh
@@ -15,14 +15,22 @@ mkdir -p totodile5/lickilicky
 mkdir totodile5/axew
 touch totodile5/omastar
 touch totodile5/zigzagoon
-echo -e 'Живет  Ocean' > cherubi5/finneon
-echo -e 'Развитые способности  Sap\nSipper' > cherubi5/blitzle
-echo -e 'Живет  Cave Desert Mountain' > darumaka0
-echo -e 'Способности\nDark Art Synchronize Trace' > gabite1/umbreon
-echo -e 'Развитые способности\nJustified' > growlithe
-echo -e 'Способности  Torrent Hyper Cutter Shell\nArmor' > krabby6
-echo -e 'Ходы  Ancientpower Bind Body Slam Dive Double-Edge\nEarth Power Icy Wind Iron Defense Knock Off Mud-Slap Rollout Seismic\nToss Sleep Talk Snore Stealth Rock Water Pulse' > totodile5/omastar
-echo -e 'Способности\nGrowl Tackle Tail Whip Headbutt Sand-Attack Odor Sleuth Mud Sport Pin\nMissile Covet Bestow Flail Rest Belly Drum Fling' > totodile5/zigzagoon
+echo 'Живет  Ocean' > cherubi5/finneon
+echo 'Развитые способности  Sap
+Sipper' > cherubi5/blitzle
+echo 'Живет  Cave Desert Mountain' > darumaka0
+echo 'Способности
+Dark Art Synchronize Trace' > gabite1/umbreon
+echo 'Развитые способности
+Justified' > growlithe8
+echo 'Способности  Torrent Hyper Cutter Shell
+Armor' > krabby6
+echo 'Ходы  Ancientpower Bind Body Slam Dive Double-Edge
+Earth Power Icy Wind Iron Defense Knock Off Mud-Slap Rollout Seismic
+Toss Sleep Talk Snore Stealth Rock Water Pulse' > totodile5/omastar
+echo 'Способности
+Growl Tackle Tail Whip Headbutt Sand-Attack Odor Sleuth Mud Sport Pin
+Missile Covet Bestow Flail Rest Belly Drum Fling' > totodile5/zigzagoon
 chmod 321 cherubi5
 chmod 752 cherubi5/roselia
 chmod 537 cherubi5/sealeo
@@ -41,3 +49,37 @@ chmod 640 totodile5/omastar
 chmod 751 totodile5/lickilicky
 chmod 044 totodile5/zigzagoon
 chmod 736 totodile5/axew
+cd ..
+chmod u+r lab0/cherubi5
+chmod u+r lab0/growlithe8
+chmod u+w lab0/totodile5
+chmod u+r lab0/totodile5/zigzagoon
+chmod u+w lab0/cherubi5/sealeo
+chmod u+r lab0/darumaka0
+ln -s ~/lab0/growlithe8 ~/lab0/cherubi5/blitzlegrowlithe
+cp lab0/krabby6 lab0/totodile5/omastarkrabby
+rm lab0/cherubi5/blitzlegrowlithe
+ln ~/lab0/growlithe8 ~/lab0/cherubi5/blitzlegrowlithe
+cat lab0/gabite1/umbreon lab0/cherubi5/blitzle > lab0/growlithe8_44
+cp -r lab0/totodile5 lab0/totodile5/axew
+ln -s ~/lab0/cherubi5 ~/lab0/Copy_3
+cp lab0/darumaka0 lab0/cherubi5/sealeo
+chmod u-r lab0/cherubi5
+chmod u-r lab0/growlithe8
+chmod u-w lab0/totodile5
+chmod u-r lab0/totodile5/zigzagoon
+chmod u-w lab0/cherubi5/sealeo
+chmod u-r lab0/darumaka0
+cd lab0
+wc -l cherubi5/finneon cherubi5/blitzle gabite1/umbreon >/tmp/lines1 2>/dev/null
+ls -ld ./**/a* 2>/tmp/err1 | tail -n2
+grep -ni 'e$' cherubi5/finneon cherubi5/blitzle gabite1/umbreon
+ls -ld ./**/a* 2>/tmp/err2  | sort -nk 2
+ls -ldt ./**/a* | head -n3
+ls cherubi5 2>/tmp/err3 | sort
+chmod -R 777 totodile5
+rm -f krabby6
+rm -f gabite1/umbreon
+rm -f cherubi5/blitzlegrowlit
+rm -rf totodile5
+rm -rf cherubi5/roselia
