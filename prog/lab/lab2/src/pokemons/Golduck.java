78f0bef092d1df0aee81f81f8b4447d75cf7f76a
simima228 <150355836+simima228@users.noreply.github.com>
2025-10-05 03:06:22 +0300

    Update Golduck.java

--- prog/lab/lab2/src/pokemons/Golduck.java
+++ prog/lab/lab2/src/pokemons/Golduck.java
@@ -4,11 +4,12 @@ import attacks.AquaJet;
 import ru.ifmo.se.pokemon.Type;
 
 
-public class Golduck extends Psyduck{
+final public class Golduck extends Psyduck{
     public Golduck(String name, int lvl) {
         super(name, lvl);
         setStats(80.0, 82.0, 78.0, 95.0, 80.0, 85.0);
         setType(Type.WATER);
         addMove(new AquaJet());
     }
+
 }
